297410ce079dbf5cdaa76c3bf7c361f94725b3b2
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-25 11:44:58 +0500

    created PrismaService

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:06062006Kamoliddin@localhost:5432/rieltor?schema=public',
+        },
+      },
+    });
+  }
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
