334474e7acbd969d02baea679ee3896616053af1
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-25 11:55:58 +0500

    connected pipes

---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateAuthDto {
+    @IsEmail()
+    readonly email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly password: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 const start = async () => {
   try {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
     const PORT = process.env.PORT || 3001;
     await app.listen(PORT, () => {
       console.log(`Server is running on port: ${PORT}`);
