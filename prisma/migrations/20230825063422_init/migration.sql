66f6d4ce456af6c81bf39fc42f9e7714124629b4
Kamoliddinbek <kamoliddinxudoyberdiyev72@gmail.com>
2023-08-25 11:35:50 +0500

    Created user on db

---
+++ prisma/migrations/20230825063422_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT,
+    "email" TEXT NOT NULL,
+    "hashed_password" TEXT NOT NULL,
+    "hashed_refresh_token" TEXT,
+    "avatar" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
